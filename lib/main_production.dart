2cdc3c515b069933178a7b2d4f25fe7a8c8024c8
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-04-05 14:13:56 +0630

    firebase config of production flavor for android

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,32 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flavors_firebase/firebase/firebase_options_prod.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
   F.appFlavor = Flavor.production;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptionsProd.currentPlatform,
+  );
+
+  // if (!kDebugMode || kProfileMode) {
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  // }
+
   runApp(const App());
 }
